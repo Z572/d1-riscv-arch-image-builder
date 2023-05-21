d4e9e568106e719270ab1744d6e6544407493c4b
Z572 <zhengjunjie@iscas.ac.cn>
2023-05-21 21:31:07 +0800

    test4

--- build_img.sh
+++ build_img.sh
@@ -1,7 +1,8 @@
 # /bin/bash
-dd if=/dev/zero of=./archlinux_riscv.img bs=1M count=2048
-sudo losetup /dev/loop17 ./archlinux_riscv.img
 docker run --name build_riscv riscv-arch-image:${1}
 docker cp build_riscv:/home/runner/work/riscv-arch-image-builder/riscv-arch-image-builder ../
+cd ../riscv-arch-image-builder
+dd if=/dev/zero of=./archlinux_riscv.img bs=1M count=2048
+sudo losetup /dev/loop17 ./archlinux_riscv.img
 sudo CI_BUILD=1 ./2_create_sd.sh /dev/loop17
 tar -zcvf archlinux_riscv.img.tar.gz archlinux_riscv.img
